a4eccd5494cad436127cced22e148c1dfcdd7df0
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-16 23:59:44 -0500

    chore: Refactor fetchMovieData function to improve API request handling

--- app/action.ts
+++ app/action.ts
@@ -3,9 +3,9 @@
 export const fetchMovieData = async (page: number) =>{
     const response = await fetch(`https://shikimori.one/api/animes?${page}&limit=8`)
 
-    console.log(response)
-
+    
     const data = await response.json()
-
+    
+    console.log(data)
     return data;
 }
