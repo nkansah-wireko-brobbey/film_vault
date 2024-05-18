e1e9cb92d92e31f7882af0f6be28f816b7dee57c
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-18 01:30:03 -0500

    chore: Update fetchMovieData function to include order parameter in API request

--- app/action.ts
+++ app/action.ts
@@ -1,7 +1,7 @@
 'use server'
 
 export const fetchMovieData = async (page: number) =>{
-    const response = await fetch(`https://shikimori.one/api/animes?${page}&limit=8`)
+    const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`)
 
     
     const data = await response.json()
