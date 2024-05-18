c77d96b9f3f19b7edcda1c3dc5935ca63edaad24
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-05-18 01:30:23 -0500

    chore: Update LoadMore component to implement lazy loading for images

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,49 +1,62 @@
-'use client'
+"use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
 
+import { fetchMovieData } from "../app/action";
 import AnimeCard, { AnimeProp } from "./AnimeCard";
-import { fetchMovieData } from "@/app/action";
 
-let page: number = 1;
+let page = 2;
+
+
 
 function LoadMore() {
-  
-  const [data, setData] = useState<AnimeProp[]>([])
-  const { ref, inView } = useInView({
-    threshold: 1,
-  });
+  const { ref, inView } = useInView();
 
+  const [data, setData] = useState<AnimeProp[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(()=>{
-    if(inView){
-      console.log("Load More")
-      fetchMovieData(page).then((res) => {
-        setData([...data,...res])
-      })
-      page++;
+  useEffect(() => {
+    if (inView) {
+      setIsLoading(true);
+      // Add a delay of 500 milliseconds
+      const delay = 500;
+
+      const timeoutId = setTimeout(() => {
+        fetchMovieData(page).then((res:AnimeProp[]) => {
+          setData([...data, ...res]);
+          page++;
+        });
+
+        setIsLoading(false);
+      }, delay);
+
+      // Clear the timeout if the component is unmounted or inView becomes false
+      return () => clearTimeout(timeoutId);
     }
-  
-  },[inView,data])
+  }, [inView, data, isLoading]);
 
   return (
     <>
-          <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index:number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          
+            <AnimeCard key={item.id} anime={item} index={index} />
+          
         ))}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
+          {inView && isLoading && (
+            <Image
+              src="./spinner.svg"
+              alt="spinner"
+              width={56}
+              height={56}
+              className="object-contain"
+            />
+          )}
         </div>
       </section>
     </>
